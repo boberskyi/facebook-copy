e5247cd6379ac6f34ef048237da0ea742256f694
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-04-20 19:59:10 +0200

    Header right styled components

---
+++ src/components/Header/HeaderRight/HeaderRight.tsx
@@ -0,0 +1,8 @@
+import {FC} from "react";
+import {HeaderRightStyled} from "./HeaderRightStyles";
+
+export const HeaderRight:FC = () => {
+  return (
+    <HeaderRightStyled></HeaderRightStyled>
+  )
+}
