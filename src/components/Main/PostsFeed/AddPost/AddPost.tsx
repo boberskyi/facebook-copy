50a49de4101aee1860399d1cc62faa3c0a623e81
Bohdan Boberskyi <boberskyi@users.noreply.github.com>
2023-05-01 08:46:54 +0200

    Create template to add post with popup

--- src/components/Main/PostsFeed/AddPost/AddPost.tsx
+++ src/components/Main/PostsFeed/AddPost/AddPost.tsx
@@ -1,29 +1,46 @@
-import styled from "styled-components";
 import {
   AddPostAvaStyled,
   AddPostBtmStyled,
   AddPostFeelingStyled,
-  AddPostInputStyled, AddPostLiveStyled, AddPostMediaStyled,
+  AddPostInputStyled,
+  AddPostLiveStyled,
+  AddPostMediaStyled, AddPostPopupButtonStyled,
+  AddPostPopupCloseStyled,
+  AddPostPopupStyled,
+  AddPostPopupTextStyled, AddPostPopupWrapStyled,
   AddPostStyled,
   AddPostTopStyled
 } from "./AddPostStyles";
-import {FC} from "react";
+import {FC, useState} from "react";
 
-export const AddPostBlock:FC = () => {
+export const AddPostBlock: FC = () => {
+  const [popupVisible, setPopupVisible] = useState(false);
+
   return (
     <AddPostStyled>
-      <AddPostTopStyled>
-        <AddPostAvaStyled />
-        <AddPostInputStyled
-          type="text"
-          placeholder="What's on your mind, Bohdan?"
-        />
+      <AddPostTopStyled onClick={() => setPopupVisible(true)}>
+        <AddPostAvaStyled/>
+        <AddPostInputStyled>
+          What's on your mind, Bohdan?
+        </AddPostInputStyled>
       </AddPostTopStyled>
       <AddPostBtmStyled>
         <AddPostLiveStyled></AddPostLiveStyled>
         <AddPostMediaStyled></AddPostMediaStyled>
         <AddPostFeelingStyled></AddPostFeelingStyled>
       </AddPostBtmStyled>
+
+      <AddPostPopupStyled visible={popupVisible}>
+        <AddPostPopupWrapStyled>
+          <AddPostPopupCloseStyled onClick={() => setPopupVisible(false)}>x</AddPostPopupCloseStyled>
+          <AddPostPopupTextStyled
+            id="post-textarea"
+            placeholder="What's on your mind, Bohdan?"
+          >
+          </AddPostPopupTextStyled>
+          <AddPostPopupButtonStyled>Publish</AddPostPopupButtonStyled>
+        </AddPostPopupWrapStyled>
+      </AddPostPopupStyled>
     </AddPostStyled>
   )
 }
--- src/components/Main/PostsFeed/AddPost/AddPostStyles.tsx
+++ src/components/Main/PostsFeed/AddPost/AddPostStyles.tsx
@@ -10,6 +10,8 @@ export const AddPostTopStyled = styled.div`
   display: flex;
   width: 100%;
   gap: 8px;
+  background-color: rgba(36,37,38,1);
+  border-radius: 8px;
 `
 export const AddPostAvaStyled = styled.img`
   min-width: 40px;
@@ -22,8 +24,16 @@ export const AddPostAvaStyled = styled.img`
   background: rgba(1,1,1,.1);
   border: none;
 `
-export const AddPostInputStyled = styled.input`
+export const AddPostInputStyled = styled.div`
   width: 100%;
+  background-color: rgba(58,59,60,.08);
+  border-radius: 20px;
+  color: rgb(28,30,31);
+  cursor: pointer;
+  font-size: 12px;
+  padding: 8px 12px;
+  display: flex;
+  align-items: center;
 `
 export const AddPostBtmStyled = styled.div`
 `
@@ -33,6 +43,41 @@ export const AddPostMediaStyled = styled.div`
 `
 export const AddPostFeelingStyled = styled.div`
 `
+interface AddPostPopupStyledProps {
+  visible: boolean
+}
+export const AddPostPopupStyled = styled.div<AddPostPopupStyledProps>`
+  display: ${props => props.visible ? 'flex' : 'none'};
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background:  rgba(0,0,0,.5);
+  z-index: 105;
+  align-items: center;
+  justify-content: center;
+`
+export const AddPostPopupWrapStyled = styled.div`
+  position: relative;
+  max-width: 500px;
+  width: 100%;
+  background-color: rgb(36,37,38);
+  display: flex;
+  flex-direction: column;
+  padding: 15px 25px;
+`
+export const AddPostPopupCloseStyled = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+`
+export const AddPostPopupTextStyled = styled.textarea`
+`
+export const AddPostPopupButtonStyled = styled.button`
+`
+
 
 
 
